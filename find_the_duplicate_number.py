afb2c6390d1c7875fceac2374cfeda387e6565c1
Kotha Sai Prathek <70202348+SaiPrathekGitam@users.noreply.github.com>
2022-05-11 17:11:13 +0530

    Create find_the_duplicate_number.py

---
+++ find_the_duplicate_number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        low = 1
+        high = len(nums) - 1 
+        while low<=high:
+            cur = (low+high)//2
+            count = sum(num<=cur for num in nums)
+            if count>cur:
+                duplicate= cur
+                high = cur-1
+            else:
+                low=cur+1
+        return duplicate
