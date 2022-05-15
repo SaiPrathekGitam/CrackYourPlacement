c7807844e58005fa56d0030d1eae384652eb3f60
Kotha Sai Prathek <70202348+SaiPrathekGitam@users.noreply.github.com>
2022-05-15 18:04:48 +0530

    Create remove_linked_list_elements.py

---
+++ remove_linked_list_elements.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        temp = head
+        while temp and temp.next:
+            if temp.next.val == val:
+                temp.next = temp.next.next
+            else:
+                temp = temp.next
+        if head and head.val == val:
+            head = head.next
+        return head
