75b72ea6988fc79bb9eecf4d7e6ae1416c9aa9c9
Kotha Sai Prathek <70202348+SaiPrathekGitam@users.noreply.github.com>
2022-06-13 18:47:03 +0530

    Create binart_tree_inorder_traversal.py

---
+++ binart_tree_inorder_traversal.py
@@ -0,0 +1,11 @@
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        def dfs(root):
+            if not root:
+                return
+            dfs(root.left)
+            res.append(root.val)
+            dfs(root.right)
+        dfs(root)
+        return res
